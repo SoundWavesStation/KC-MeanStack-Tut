7e6e95add29477cb14a1acf86184998715e75142
Kyam Collados <Kyam.Collados@cireson.com>
2018-12-29 05:59:24 +0800

    Added Post List and Header Template and Components

---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@ Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls : ['./post-list.component.css']
+})
+export class PostListComponent {
+  /*posts = [
+    {title: 'First post' , content: 'This is the first post content'},
+    {title: 'Second post', content: 'This is the Second post content'},
+    {title: 'Third post', content: 'This is the Third post content'}
+  ];*/
+
+  posts = [];
+
+}
